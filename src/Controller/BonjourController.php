9abf00a8bc010efc21009b96006fa93ed2681074
SpinBox <quentin.mimault@orange.fr>
2020-10-03 17:11:39 +0200

    second commit

--- src/Controller/BonjourController.php
+++ src/Controller/BonjourController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BonjourController extends AbstractController
@@ -20,5 +25,19 @@ class BonjourController extends AbstractController
         ]);
     }
 
+    function createUserForm(){
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ])
+            ->getForm();
+
+        return $this->render('bonjour/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
 }
